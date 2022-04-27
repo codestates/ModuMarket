d064d65f9f6ce47d35b917644fe43c2409d6d597
cookiemiro <xoml9598@gmail.com>
2022-04-28 05:32:00 +0900

    Feat: image file save

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,31 @@
 const router = require('express').Router();
 const controller = require('../controllers');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    } else {
+        cb(null, false);
+    }
+}
+  
+const upload = multer({ 
+    storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 // 마이페이지 유저정보 가져오기
 router.get('/', controller.user.mypage);
@@ -15,6 +41,12 @@ router.post('/', controller.user.auth);
 
 // 사는곳 비밀번호 수정
 router.patch('/', controller.user.changeInfo);
+
+// 이미지 등록
+router.post('/:email/image', upload.single('img'), controller.user.uploadImage);
+
+// 이미지 가져오기
+router.get('/:email/image', controller.user.getImage)
 
 // 회원탈퇴
 router.delete('/', controller.user.deleteInfo);
