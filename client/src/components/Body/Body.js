aedb8eceac06401dc83e62d64886d0463d12ec13
HyeongAn <guddks84@gamil.com>
2022-04-29 10:25:30 +0900

    Feat adding mypage

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -3,6 +3,7 @@ import Main from '../../pages/MainPage/Main'
 import Signup from '../Modals/Signup/Signup'
 import Login from '../Modals/Login/Login'
 import BoardPage from '../../pages/BoardPage/BoardPage'
+import MyPage from '../../pages/MyPage/MyPage'
 
 
 const Body = () => {
@@ -13,6 +14,7 @@ const Body = () => {
             <Route path="/board" element={<BoardPage />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<MyPage />} />
         </Routes>
     )
 }
