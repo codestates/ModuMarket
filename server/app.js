bcb600e88a0509aee39a5602f7a19a041d6de09f
parkbomi-77 <vnstn785@naver.com>
2022-05-02 16:21:09 +0900

    Chore:merge

--- server/app.js
+++ server/app.js
@@ -17,7 +17,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/images', express.static('images'))
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+  method: ['get', 'post', 'options', 'delete']
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', indexRouter);
